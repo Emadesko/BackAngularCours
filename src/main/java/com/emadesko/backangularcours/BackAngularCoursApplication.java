2d2059184ed0e113275c7092b11c81d94eedd8eb
Emadesko <assilaedem1@gmail.com>
2025-02-28 14:40:22 +0000

    first commit

---
+++ src/main/java/com/emadesko/backangularcours/BackAngularCoursApplication.java
@@ -0,0 +1,13 @@
+package com.emadesko.backangularcours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAngularCoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackAngularCoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emadesko/backangularcours/BackAngularCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emadesko.backangularcours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAngularCoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
